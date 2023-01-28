de3d11cc310c1f639e3990f58f10ff7a64136a45
seokzin <seokzin95@gmail.com>
2023-01-28 23:18:26 +0900

    fix(head): remove typo

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ const Head = () => {
       <title>8-8</title>
       <link rel="icon" href="/images/favicon.svg" type="image/svg+xml" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="DND 8-8" />{' '}
+      <meta name="description" content="DND 8-8" />
     </>
   )
 }
