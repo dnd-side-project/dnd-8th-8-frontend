ed145ed0b288f2d72d3825e8fa6560156088fd73
seokzin <seokzin95@gmail.com>
2023-01-29 18:31:55 +0900

    refactor(layout): combine styledLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,23 @@
 'use client'
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useServerInsertedHTML } from 'next/navigation'
 import { RecoilRoot } from 'recoil'
 import { ThemeProvider } from 'styled-components'
 
 import { Navbar } from '@/components'
+import { useStyledComponentsRegistry } from '@/hooks'
 import { GlobalStyle, theme } from '@/styles'
 
-import RootStyleLayout from './styleLayout'
+const RootStyleLayout = ({ children }: { children: React.ReactNode }) => {
+  const [StyledComponentsRegistry, styledComponentsFlushEffect] =
+    useStyledComponentsRegistry()
+
+  useServerInsertedHTML(() => <>{styledComponentsFlushEffect()}</>)
+
+  return <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+}
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   const queryClient = new QueryClient()
