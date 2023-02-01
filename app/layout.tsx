699c4433589970c7077b56e27daf236459378763
seokzin <seokzin95@gmail.com>
2023-02-01 23:19:20 +0900

    style(layout): set viewport width range

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useServerInsertedHTML } from 'next/navigation'
 import { RecoilRoot } from 'recoil'
-import { ThemeProvider } from 'styled-components'
+import styled, { ThemeProvider } from 'styled-components'
 
 import { Navbar } from '@/components'
 import { useStyledComponentsRegistry } from '@/hooks'
@@ -34,11 +34,11 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
               <ThemeProvider theme={theme}>
                 <GlobalStyle />
 
-                <div>
+                <MobileLayout>
                   <Navbar />
 
                   {children}
-                </div>
+                </MobileLayout>
               </ThemeProvider>
             </RootStyleLayout>
           </RecoilRoot>
@@ -49,3 +49,12 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
 }
 
 export default RootLayout
+
+const MobileLayout = styled.div`
+  min-width: 360px;
+  max-width: 480px;
+  min-height: 100vh;
+
+  /* TODO : 개발 목적. 완료시 삭제 예정 */
+  border: 1px solid red;
+`
