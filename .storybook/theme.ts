d0ce0dd6453fd717292e26b6493c0480467d6554
seokzin <seokzin95@gmail.com>
2023-02-11 22:49:55 +0900

    feat(storybook): add logo to storybook theme

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -1,15 +1,29 @@
 import { create } from '@storybook/theming'
 
 const theme = {
+  brandTitle: 'Wedding map',
   fontBase: 'pretendard, sans-serif',
 }
 
 export const light = create({
   ...theme,
   base: 'light',
+  brandImage: '/images/logo.light.svg',
+
+  colorPrimary: '#F693E1',
+  colorSecondary: '#192EA1',
 })
 
 export const dark = create({
   ...theme,
   base: 'dark',
+  brandImage: '/images/logo.dark.svg',
+
+  colorPrimary: '#192EA1',
+  colorSecondary: '#F693E1',
+
+  appBg: '#0D1117',
+  barBg: '#161B22',
+  appContentBg: '#161B22',
+  inputBg: '#0D1117',
 })
