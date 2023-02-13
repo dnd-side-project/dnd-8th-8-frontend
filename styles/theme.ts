eafb1b6eeb3d7984a708849757262ecc540b9703
seokzin <seokzin95@gmail.com>
2023-02-13 23:42:35 +0900

    feat(wds): set theme system
    - 디자인시스템 구축되면 리팩터링 예정

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,52 +1,69 @@
-const dark = {
-  mainBackground: '#292B2E',
-  primaryText: '#fff',
-  secondaryText: 'rgba(255,255,255,0.45)',
-  disable: 'rgba(255,255,255,0.25)',
-  border: '#d1d5da',
-  divider: 'rgba(255, 255, 255, 0.6)',
-  background: 'rgb(217, 223, 226)',
-  tableHeader: 'rgba(255,255,255,0.02)',
-  themeIcon: '#FBE302',
-  blue1: '#f1f8ff',
-  blue2: '#c0d3eb',
-  blue3: '#00adb5',
-  green: '#1fab89',
-  gray: '#393e46',
+const color = {
+  neutral0: '#ffffff',
+  neutral100: '#f4f4f4',
+  neutral300: '#d8dae5',
+  neutral400: '#cdcfdc',
+  neutral500: '#b4b6c5',
+  neutral800: '#474d66',
+  neutral900: '#0f0e2d',
+
+  primary0: '#fff6fd',
+  primary500: '#F693E1',
+  primary900: '#900072',
+
+  secondary0: '#EAEDFF',
+  secondary500: '#283FBC',
+  secondary900: '#192EA1',
 }
 
-const light = {
-  mainBackground: '#fff',
-  primaryText: '#292B2E',
-  secondaryText: 'rgba(0, 0, 0, 0.45)',
-  disable: 'rgba(0, 0, 0, 0.25)',
-  border: '#d1d5da',
-  divider: 'rgba(106, 115, 125, 0.3)',
-  background: 'rgb(217, 223, 226)',
-  tableHeader: 'rgba(0, 0, 0, 0.02)',
-  themeIcon: '#1fab89',
-  blue1: '#f1f8ff',
-  blue2: '#c0d3eb',
-  blue3: '#00adb5',
-  green: '#1fab89',
-  gray: '#393e46',
+const fontSize = {
+  h1: 40,
+  h2: 30,
+  h3: 24,
+  h4: 18,
+  h5: 14,
+  t1: 24,
+  t2: 18,
+  t3: 16,
+  t4: 14,
+  t5: 12,
 }
 
-const theme = (mode: 'dark' | 'light') => ({
-  color: mode === 'dark' ? dark : light,
-  fontSize: {
-    xs: '10px',
-    sm: '12px',
-    md: '16px',
-    lg: '20px',
-    xl: '24px',
-  },
-  fontWeight: {
-    regular: 400,
-    semiBold: 600,
-    bold: 700,
-    extraBold: 800,
-  },
-})
+const lineHeight = {
+  h1: 64,
+  h2: 36,
+  h3: 32,
+  h4: 24,
+  h5: 18,
+  t1: 32,
+  t2: 24,
+  t3: 24,
+  t4: 20,
+  t5: 18,
+}
+
+const MEDIUM = 500
+const SEMI_BOLD = 600
+const BOLD = 700
+
+const fontWeight = {
+  h1: SEMI_BOLD,
+  h2: BOLD,
+  h3: SEMI_BOLD,
+  h4: SEMI_BOLD,
+  h5: SEMI_BOLD,
+  t1: MEDIUM,
+  t2: MEDIUM,
+  t3: MEDIUM,
+  t4: MEDIUM,
+  t5: MEDIUM,
+}
+
+const theme = {
+  color,
+  fontSize,
+  fontWeight,
+  lineHeight,
+}
 
 export default theme
--- types/styled.d.ts
+++ types/styled.d.ts
@@ -1,7 +1,57 @@
 import 'styled-components'
 
-import { ThemeType } from '../styles/theme'
-
 declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
+  export interface DefaultTheme {
+    color: {
+      neutral0: string
+      neutral100: string
+      neutral300: string
+      neutral400: string
+      neutral500: string
+      neutral800: string
+      neutral900: string
+      primary0: string
+      primary500: string
+      primary900: string
+      secondary0: string
+      secondary500: string
+      secondary900: string
+    }
+    fontSize: {
+      h1: number
+      h2: number
+      h3: number
+      h4: number
+      h5: number
+      t1: number
+      t2: number
+      t3: number
+      t4: number
+      t5: number
+    }
+    fontWeight: {
+      h1: number
+      h2: number
+      h3: number
+      h4: number
+      h5: number
+      t1: number
+      t2: number
+      t3: number
+      t4: number
+      t5: number
+    }
+    lineHeight: {
+      h1: number
+      h2: number
+      h3: number
+      h4: number
+      h5: number
+      t1: number
+      t2: number
+      t3: number
+      t4: number
+      t5: number
+    }
+  }
 }
