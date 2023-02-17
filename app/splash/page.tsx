985fa6ac407218f1299f5fabfe446624f44451af
chldmswnl <chldmswnl12@gmail.com>
2023-02-17 12:17:20 +0900

    feat(page): add animation in splash page

--- app/splash/page.tsx
+++ app/splash/page.tsx
@@ -2,22 +2,28 @@
 
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
-import styled from 'styled-components'
+import { useEffect, useState } from 'react'
+import styled, { css, keyframes } from 'styled-components'
 
 const Splash = () => {
   const router = useRouter()
-
+  const [startFadeoutAnimation, setStartFadeOutAnimation] =
+    useState<boolean>(false)
   useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push('/login')
-    }, 2500)
+    let timer: NodeJS.Timeout
+    new Promise(function (resolve) {
+      timer = setTimeout(() => {
+        setStartFadeOutAnimation(true)
+        resolve('success')
+      }, 1500)
+    }).then(() => router.push('/login'))
+
     return () => clearTimeout(timer)
   }, [])
 
   return (
-    <SplashPageStyle>
-      <SplashLogoWrapper>
+    <Layout startFadeoutAnimation={startFadeoutAnimation}>
+      <LogoWrapper>
         <Image
           src="/images/logo.splash.svg"
           alt="splash logo"
@@ -25,21 +31,36 @@ const Splash = () => {
           sizes="100%"
           priority
         />
-      </SplashLogoWrapper>
-    </SplashPageStyle>
+      </LogoWrapper>
+    </Layout>
   )
 }
 
-const SplashPageStyle = styled.div`
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+
+  to {
+    opacity: 0;
+  }
+`
+
+const Layout = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 100vh;
+  height: 100%;
   background: linear-gradient(194.48deg, #1f38bc -9.76%, #415ae2 88.62%);
+  animation: ${(props: { startFadeoutAnimation: boolean }) =>
+    props.startFadeoutAnimation &&
+    css`
+      ${fadeOut} .5s linear forwards
+    `};
 `
 
-const SplashLogoWrapper = styled.div`
+const LogoWrapper = styled.div`
   position: relative;
   width: 7.5rem;
   height: 10.4rem;
