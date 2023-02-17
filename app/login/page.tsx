4f97e25ee73b3eb2937fd7f5a9211a2892797148
chldmswnl <chldmswnl12@gmail.com>
2023-02-17 10:10:57 +0900

    feat(page): work with login page UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,58 @@
+'use client'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import styled from 'styled-components'
+
 const Login = () => {
-  return <div>Login page</div>
+  const router = useRouter()
+  return (
+    <Layout>
+      <LogoWrapper>
+        <Image
+          alt="login logo"
+          src="/images/logo.login.svg"
+          fill
+          sizes="100%"
+          priority
+        />
+      </LogoWrapper>
+      <ButtonsWrapper>
+        <button onClick={() => router.push('/onboarding/gender')}>
+          Google Login
+        </button>
+        <button onClick={() => router.push('/onboarding/gender')}>
+          Kakao Login
+        </button>
+      </ButtonsWrapper>
+    </Layout>
+  )
 }
+
+const Layout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding-top: 10rem;
+`
+
+const LogoWrapper = styled.div`
+  position: relative;
+  width: 8.5rem;
+  height: 11.7rem;
+  margin-bottom: 15.7rem;
+`
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  button:nth-child(1) {
+    margin-bottom: 1rem;
+  }
+`
 
 export default Login
