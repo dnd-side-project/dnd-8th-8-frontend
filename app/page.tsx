104930cda9f503f2e550950aac643d92af7e0324
chldmswnl <chldmswnl12@gmail.com>
2023-02-17 09:19:21 +0900

    feat(page): work with splash page UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import styled from 'styled-components'
 
 const Page = () => {
+  const router = useRouter()
+  useEffect(() => {
+    router.push('/splash')
+  }, [])
   return (
     <Layout>
       <p>home</p>
--- app/splash/page.tsx
+++ app/splash/page.tsx
@@ -1,5 +1,48 @@
+'use client'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import styled from 'styled-components'
+
 const Splash = () => {
-  return <div>Splash page</div>
+  const router = useRouter()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/login')
+    }, 2500)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <SplashPageStyle>
+      <SplashLogoWrapper>
+        <Image
+          src="/images/logo.splash.svg"
+          alt="splash logo"
+          fill
+          sizes="100%"
+          priority
+        />
+      </SplashLogoWrapper>
+    </SplashPageStyle>
+  )
 }
+
+const SplashPageStyle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(194.48deg, #1f38bc -9.76%, #415ae2 88.62%);
+`
+
+const SplashLogoWrapper = styled.div`
+  position: relative;
+  width: 7.5rem;
+  height: 10.4rem;
+`
 
 export default Splash
