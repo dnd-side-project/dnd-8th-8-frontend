fce923fc5f4b972fc16a4144a88f7bef0fb393e0
seokzin <seokzin95@gmail.com>
2023-02-21 01:33:57 +0900

    style(text): set white-space option

--- components/Text/Text.styled.ts
+++ components/Text/Text.styled.ts
@@ -6,4 +6,5 @@ export const Layout = styled.span<TextProps>`
   font-weight: ${({ theme, as = 't3' }) => theme.fontWeight[as]};
   line-height: ${({ theme, as = 't3' }) => theme.lineHeight[as]}px;
   color: ${({ theme, color = 'neutral900' }) => theme.color[color]};
+  white-space: pre-wrap;
 `
