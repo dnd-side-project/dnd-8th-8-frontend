8c17c8b4408dfac28ddb8e653c4c698ff2b5ec84
seokzin <seokzin95@gmail.com>
2023-02-23 23:19:01 +0900

    refactor(prettier): apply prettier

--- components/Footer/Footer.styled.ts
+++ components/Footer/Footer.styled.ts
@@ -3,25 +3,23 @@ import styled from 'styled-components'
 export const Layout = styled.footer`
   position: fixed;
   bottom: 0;
-  height: 60px;
-
   display: flex;
-  justify-content: space-around;
   align-items: center;
-
+  justify-content: space-around;
   width: 100%;
+  height: 60px;
   padding: 0;
-  box-shadow: 6px 4px 18px 3px rgba(0, 0, 0, 0.11);
+  box-shadow: 6px 4px 18px 3px rgb(0 0 0 / 11%);
 
   a {
-    text-decoration: none;
     width: 100%;
+    text-decoration: none;
   }
 `
 
 export const ItemBox = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
   gap: 4px;
+  align-items: center;
 `
