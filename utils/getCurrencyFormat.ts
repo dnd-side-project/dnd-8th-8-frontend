fa806ecc0b8ca3ba86292073ab20ed355f1efba1
seokzin <seokzin95@gmail.com>
2023-02-24 02:46:14 +0900

    refactor(util): change to arrow function

--- utils/getCurrencyFormat.ts
+++ utils/getCurrencyFormat.ts
@@ -1,5 +1,4 @@
-function getCurrencyFormat(number: number) {
-  return new Intl.NumberFormat('ko-KR').format(number)
-}
+const getCurrencyFormat = (number: number) =>
+  new Intl.NumberFormat('ko-KR').format(number)
 
 export default getCurrencyFormat
