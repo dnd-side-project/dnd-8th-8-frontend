633bdb1faa72738f136aa717f8878bcd88f16e3d
chldmswnl <chldmswnl12@gmail.com>
2023-02-25 12:19:57 +0900

    feat(util): create constant file

---
+++ constant/index.ts
@@ -0,0 +1,15 @@
+export const QUERY_KEYS = {
+  LOGIN: {
+    KAKAO_LOGIN: 'kakao_login',
+  },
+}
+
+export const PAGE_URL = {
+  HOME: '/home',
+  CHECKLIST: '/checklist',
+  COLLECTION: '/collection',
+  COMMUNITY: '/community',
+  MYPAGE: '/my-page',
+}
+
+export const FOOTER_HEIGHT = '6rem'
