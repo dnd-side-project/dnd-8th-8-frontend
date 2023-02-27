dc4b408ac49374a6141554cffa3c523ffa6f5d40
chldmswnl <chldmswnl12@gmail.com>
2023-02-27 23:19:43 +0900

    feat(axios): change header options

--- api/config/axiosInstance.ts
+++ api/config/axiosInstance.ts
@@ -2,7 +2,12 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  withCredentials: true,
+  headers: {
+    'Content-Type': `application/json;charset=UTF-8`,
+    Accept: 'application/json',
+    'Access-Control-Allow-Origin': 'http://localhost:3000',
+    withCredentials: true,
+  },
 })
 
 axiosInstance.interceptors.request.use(
