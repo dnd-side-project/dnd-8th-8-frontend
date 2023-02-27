cbac4dc6cde34b6d8acbaab598b6ad34c6782484
chldmswnl <chldmswnl12@gmail.com>
2023-02-27 23:28:20 +0900

    feat(Header): move codes to proper files

--- components/Header/Header.styled.ts
+++ components/Header/Header.styled.ts
@@ -1,3 +1,27 @@
 import styled from 'styled-components'
 
-export const Layout = styled.nav``
+export const HeaderLayout = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  height: 4rem;
+  margin-bottom: 1rem;
+`
+
+export const BtnSection = styled.div`
+  display: flex;
+  align-items: center;
+  min-width: 2rem;
+  height: 100%;
+  cursor: pointer;
+`
+
+export const TitleSection = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+export const HeaderButton = styled.button`
+  background-color: transparent;
+  border: none;
+`
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,10 @@
-import { Icon, Text } from '@/components'
-import styled from 'styled-components'
-export interface HeaderProps {
-  hideBackBtn?: boolean
-  handleBackBtnClick?: () => void
-  rightBtn?: React.ReactNode
-  title?: string
-}
+import { HeaderProps, Icon, Text } from '@/components'
+import {
+  BtnSection,
+  HeaderButton,
+  HeaderLayout,
+  TitleSection,
+} from './Header.styled'
 
 const Header = ({
   handleBackBtnClick,
@@ -33,29 +32,3 @@ const Header = ({
 }
 
 export default Header
-
-const HeaderLayout = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  height: 4rem;
-  margin-bottom: 1rem;
-`
-
-const BtnSection = styled.div`
-  display: flex;
-  align-items: center;
-  min-width: 2rem;
-  height: 100%;
-  cursor: pointer;
-`
-
-const TitleSection = styled.div`
-  display: flex;
-  align-items: center;
-`
-
-const HeaderButton = styled.button`
-  background-color: transparent;
-  border: none;
-`
