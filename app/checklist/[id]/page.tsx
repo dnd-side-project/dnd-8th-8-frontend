686e37170f0319393ad52586056bb51ebce2eab0
seokzin <seokzin95@gmail.com>
2023-03-07 23:24:08 +0900

    feat(checklist/detail): add header component

--- app/checklist/[id]/page.tsx
+++ app/checklist/[id]/page.tsx
@@ -1,8 +1,32 @@
+'use client'
+
+import { Header, Text } from '@/components'
 import { ParamsProps } from '@/types/param'
+import { useRouter } from 'next/navigation'
+import styled from 'styled-components'
 
 const DetailChecklist = ({ params: { id } }: ParamsProps) => {
-  console.log(id)
-  return <div>checklist page</div>
+  const router = useRouter()
+
+  return (
+    <Layout>
+      <Header
+        handleBackBtnClick={() => router.push('/checklist')}
+        rightBtn={
+          <div onClick={() => null}>
+            <Text as="t4" color="neutral800">
+              편집
+            </Text>
+          </div>
+        }
+        title="체크리스트 작성"
+      />
+    </Layout>
+  )
 }
 
 export default DetailChecklist
+
+const Layout = styled.div`
+  padding: 2rem;
+`
