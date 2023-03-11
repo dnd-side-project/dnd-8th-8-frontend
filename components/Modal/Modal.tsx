476f250270f1632911bdd319b7e113843c6d94ed
seokzin <seokzin95@gmail.com>
2023-03-11 22:58:52 +0900

    feat(modal): stop event propagation when modal open

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -1,14 +1,34 @@
+import { useEffect } from 'react'
 import { Content, Overlay } from './Modal.styled'
 
 export interface ModalProps {
   children: React.ReactNode
   onClose: () => void
+  isOpen: boolean
 }
 
-const Modal = ({ children, onClose }: ModalProps) => {
+const Modal = ({ isOpen, children, onClose }: ModalProps) => {
+  const handleTouchMove = (e: TouchEvent) => {
+    e.stopPropagation()
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden'
+      document.addEventListener('touchmove', handleTouchMove)
+    } else {
+      document.body.style.overflow = 'null'
+      document.removeEventListener('touchmove', handleTouchMove)
+    }
+    return () => {
+      document.body.style.overflow = 'null'
+      document.removeEventListener('touchmove', handleTouchMove)
+    }
+  }, [isOpen])
+
   return (
     <Overlay onClick={onClose}>
-      <Content>{children}</Content>
+      <Content onClick={handleTouchMove}>{children}</Content>
     </Overlay>
   )
 }
