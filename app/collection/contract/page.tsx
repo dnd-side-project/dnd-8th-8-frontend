08faedb53a77a5971d8c73bc1865d7f0d6624261
chldmswnl <chldmswnl12@gmail.com>
2023-03-29 23:31:19 +0900

    feat(contract): work with Contract UI

--- app/collection/contract/page.tsx
+++ app/collection/contract/page.tsx
@@ -2,6 +2,7 @@
 
 import { FloatingButton, Input, Tab } from '@/components'
 import { useGetContractList } from '@/queries/contract/useGetContract'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import styled from 'styled-components'
 import ContractCard from './components/contractCard'
@@ -12,36 +13,50 @@ const Contract = () => {
     { label: '계약서', path: '/collection/contract' },
   ]
   const router = useRouter()
-  const { data } = useGetContractList()
+  const { data, isLoading } = useGetContractList()
 
   return (
     <>
-      <Tab tabs={tabs} />
-      <Layout>
-        <Input
-          placeholder="검색어를 입력해주세요."
-          handleChange={(e) => console.log(e.currentTarget.value)}
-          searchIcon
-          borderStyle
-        />
-        <CardSection>
-          {data?.data.length ? (
-            data.data.map((item) => (
-              <ContractCard
-                cardTheme="normal"
-                contractInfo={item}
-                key={item.id}
-              />
-            ))
-          ) : (
-            <ContractCard cardTheme="skeleton" />
-          )}
-        </CardSection>
-        <FloatingButton
-          icon={'plus'}
-          onClick={() => router.push('/collection/contract/create')}
-        />
-      </Layout>
+      {isLoading ? (
+        <LoadingSection>
+          <Image
+            alt="loading spinner"
+            src="/images/loading-spinner.svg"
+            width={150}
+            height={150}
+            priority
+          />
+        </LoadingSection>
+      ) : (
+        <>
+          <Tab tabs={tabs} />
+          <Layout>
+            <Input
+              placeholder="검색어를 입력해주세요."
+              handleChange={(e) => console.log(e.currentTarget.value)}
+              searchIcon
+              borderStyle
+            />
+            <CardSection>
+              {data?.data.length ? (
+                data.data.map((item) => (
+                  <ContractCard
+                    cardTheme="normal"
+                    contractInfo={item}
+                    key={item.id}
+                  />
+                ))
+              ) : (
+                <ContractCard cardTheme="skeleton" />
+              )}
+            </CardSection>
+            <FloatingButton
+              icon={'plus'}
+              onClick={() => router.push('/collection/contract/create')}
+            />
+          </Layout>
+        </>
+      )}
     </>
   )
 }
@@ -57,4 +72,11 @@ const Layout = styled.div`
 const CardSection = styled.div`
   row-gap: 1rem;
   margin-top: 2rem;
+`
+const LoadingSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
 `
